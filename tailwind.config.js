c6d4b374541c1d79c73845849bff48bdc2c096ce
abdullah107189 <abdullah107189@gmail.com>
2024-08-19 12:55:01 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// module.exports = {
+//     // content: ["'./index.html',./src/**/*.{html,js}"],
+//     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+//     theme: {
+//         fontFamily: {
+//             'manrope': ['Manrope', 'sans-serif'],
+//             'jersey': ['"Jersey 10"', 'sans-serif'],
+//         }
+//     }
+// }
